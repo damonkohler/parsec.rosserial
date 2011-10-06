850fc5c9a4b3d848ef3c0fbeef35f66e2178e9bb
Damon Kohler <damonkohler@google.com>
2011-10-06 16:22:11 +0200

    Initial commit of rosserial_java fork. Lots of refactoring and general cleanup. Added tests.

---
+++ rosserial_java/src/main/java/org/ros/rosserial/Proxy.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros.rosserial;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import org.ros.message.Message;
+import org.ros.message.MessageListener;
+import org.ros.node.Node;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+/**
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+class Proxy {
+
+	/**
+	 * Node hosting the subscribers and publishers.
+	 */
+	private final Node node;
+
+	/**
+	 * Topic ID to publisher.
+	 * 
+	 * The {@link Publisher}s are parameterized with {@link Object} so that we
+	 * can handle arbitrary message definition classes (e.g. new style or old
+	 * style messages).
+	 */
+	private final Map<Integer, Publisher<Object>> publishers;
+
+	/**
+	 * Topic ID to subscriber.
+	 */
+	private final Map<Integer, Subscriber<?>> subscribers;
+
+	public Proxy(Node node) {
+		this.node = node;
+		publishers = Maps.newHashMap();
+		subscribers = Maps.newHashMap();
+	}
+
+	/**
+	 * Registers a publisher being transmitted over the serial port.
+	 * 
+	 * @param topic
+	 *            the TopicInfo message describing the topic
+	 */
+	public void registerPublisher(int topicId, String topicName,
+			String messageType) {
+		Publisher<Object> publisher = node.newPublisher(topicName, messageType);
+		getPublishers().put(topicId, publisher);
+		node.getLog().info(
+				"Publishing to " + topicName + " (" + messageType + ").");
+	}
+
+	/**
+	 * Registers a subscriber being transmitted over the serial port.
+	 * 
+	 * @param topic
+	 *            the TopicInfo message describing the topic
+	 */
+	public void registerSubscriber(int topicId, String topicName,
+			String messageType, MessageListener<?> listener) {
+		Subscriber<?> subscriber = node.newSubscriber(topicName, messageType,
+				listener);
+		getSubscribers().put(topicId, subscriber);
+		node.getLog().info(
+				"Subscribed to " + topicName + " (" + messageType + ").");
+	}
+
+	public void publish(int topicId, Message message) {
+		getPublishers().get(topicId).publish(message);
+	}
+
+	public Map<Integer, Publisher<Object>> getPublishers() {
+		return publishers;
+	}
+
+	public Map<Integer, Subscriber<?>> getSubscribers() {
+		return subscribers;
+	}
+}
---
+++ rosserial_java/src/main/java/org/ros/rosserial/RosSerial.java
@@ -0,0 +1,141 @@
+// Software License Agreement (BSD License)
+//
+// Copyright (c) 2011, Willow Garage, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of Willow Garage, Inc. nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package org.ros.rosserial;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.BufferOverflowException;
+
+import com.google.common.base.Preconditions;
+
+import org.ros.exception.RosRuntimeException;
+import org.ros.node.DefaultNodeFactory;
+import org.ros.node.Node;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMain;
+
+/**
+ * The host endpoint for a rosserial connection.
+ * 
+ * @author adasta@gmail.com (Adam Stambler)
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+public class RosSerial implements NodeMain {
+
+	private static final int STREAM_BUFFER_SIZE = 8192;
+
+	/**
+	 * Output stream for the serial line used for communication.
+	 */
+	private final OutputStream outputStream;
+
+	/**
+	 * Input stream for the serial line used for communication.
+	 */
+	private final InputStream inputStream;
+
+	/**
+	 * The node which is hosting the publishers and subscribers.
+	 */
+	private Node node;
+
+	private Protocol protocol;
+
+	/**
+	 * * It is not necessary to provide buffered streams. Buffering is handled
+	 * internally.
+	 * 
+	 * @param inputStream
+	 *            the {@link InputStream} for the connected device
+	 * @param outputStream
+	 *            the {@link OutputStream} for the connected device
+	 */
+	public RosSerial(InputStream inputStream, OutputStream outputStream) {
+		this.inputStream = new BufferedInputStream(inputStream,
+				STREAM_BUFFER_SIZE);
+		this.outputStream = new BufferedOutputStream(outputStream,
+				STREAM_BUFFER_SIZE);
+	}
+
+	@Override
+	public void main(NodeConfiguration nodeConfiguration) {
+		Preconditions.checkState(node == null);
+		DefaultNodeFactory nodeFactory = new DefaultNodeFactory();
+		node = nodeFactory.newNode("rosserial_node", nodeConfiguration);
+		PacketSender packetSender = new DefaultPacketSender(outputStream,
+				node.getLog());
+		protocol = new Protocol(node, packetSender);
+		PacketReceiver packetReceiver = new DefaultPacketReceiver(protocol);
+		PacketBuilder packetBuilder = new PacketBuilder(packetReceiver);
+		protocol.start();
+
+		while (true) {
+			try {
+				int inputByte = inputStream.read();
+				if (inputByte == -1) {
+					// The connection has been closed.
+					break;
+				}
+				packetBuilder.addByte((byte) inputByte);
+			} catch (IllegalStateException e) {
+				node.getLog().error("Protocol error.", e);
+			} catch (IOException e) {
+				throw new RosRuntimeException(e);
+			} catch (BufferOverflowException e) {
+				// TODO(damonkohler): Because there is no checksum on the data
+				// length, it's possible to try to read too little or too much
+				// data for the current packet.
+				throw new RosRuntimeException(e);
+			}
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		protocol.shutdown();
+		node.shutdown();
+		try {
+			inputStream.close();
+		} catch (IOException e) {
+			node.getLog().error("IO error while shutting down.", e);
+		}
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+			node.getLog().error("IO error while shutting down.", e);
+		}
+	}
+}
