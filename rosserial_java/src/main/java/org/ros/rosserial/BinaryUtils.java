79b145a5651fa8efff54edc978dbde423297cc01
Damon Kohler <damonkohler@google.com>
2011-10-10 08:45:56 +0200

    Remove unused constant.

--- rosserial_java/src/main/java/org/ros/rosserial/BinaryUtils.java
+++ rosserial_java/src/main/java/org/ros/rosserial/BinaryUtils.java
@@ -37,14 +37,7 @@ package org.ros.rosserial;
  * 
  * @author Adam Stambler
  */
-public class BinaryUtils {
-
-	/**
-	 * A mapping of single digit integers to their hex digit.
-	 */
-	public static final char[] DIGIT_TO_CHAR = { '0', '1', '2', '3', '4', '5',
-			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
+class BinaryUtils {
 	/**
 	 * Converts a Byte array into a string representation.
 	 * 
